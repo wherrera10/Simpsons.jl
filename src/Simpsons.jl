63ba5694bb6a8e7cc02e6ec4053805a78a644f7f
Bill <wherrera10@users.noreply.github.com>
2021-05-26 19:13:27 -1000

    Update Simpsons.jl

--- src/Simpsons.jl
+++ src/Simpsons.jl
@@ -1,3 +1,4 @@
+""" Julia module to check data for a Simpson's statistical paradox """
 module Simpsons
 
 export has_simpsons_paradox, make_paradox, plot_clusters, plot_kmeans_by_factor, simpsons_analysis
